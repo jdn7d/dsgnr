f7b07aeff41b4cd8505658bd1c49a83fa47248ba
Jessie <jssdnvn.com>
2020-11-23 19:17:53 -0600

    added show method for designer and show view

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -5,8 +5,23 @@ class DesignersController < ApplicationController
    end
 
    def create
+      @designer = Designer.new(designer_params)
+      if @designer.save
+         session[:designer_id] = @designer.id
+         redirect_to @designer
+      else 
+         render :new
+      end   
    end
    
    def show
+      @designer = Designer.find(params[:id] )
    end
+
+   private
+
+   def designer_params
+      params.require(:designer).permit(:username, :email, :password)
+   end
+
 end
