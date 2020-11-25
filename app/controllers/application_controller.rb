d1dc8045094c4d8c8989d4fa8492d0c1d3b13c68
Jessie <jssdnvn.com>
2020-11-24 21:57:02 -0600

    created the room/new with partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
 
    def current_designer
-      if (designer_id == session[:designer_id])
-         @current_designer 
-      session[:designer_id] = @designer.id
+      @current_designer ||= User.find_by(id: session[:user])
    end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,25 @@
 class RoomsController < ApplicationController
+   
+   def index 
+      @rooms = Room.all
+   end
+
+   def new 
+      @room = Room.new
+   end
+
+   def create
+     @room = Room.create(room_params)
+     redirect_to room_path(@room)
+   end
+
+   def show 
+   end
+
+   private
+
+   def room_params
+      params.require(:room).permit(:name, :wall_color, :floor_color, :light)
+   end
+
 end
