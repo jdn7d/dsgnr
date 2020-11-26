3a5ffdcc2fb5c6b5aeb99a9ba99b17f21b23bcad
Jessie <jssdnvn.com>
2020-11-26 17:13:55 -0600

    added all views for pieces and added pieces/form with new form

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,33 @@
 class PiecesController < ApplicationController
+   
+   def index 
+      @designer = Designer.find(session[:designer_id])
+      @pieces = @designer.pieces
+   end
+
+   def new
+      @piece = Piece.new
+   end
+
+   def create
+
+   end
+
+   def show  
+   end
+
+   def edit
+   end
+
+   def update  
+   end
+
+   def destroy 
+
+   end
+
+   def piece_params
+      params.require(:piece).permit(:name, :category, :style, :quantity, :room_name, :store_name)
+   end
+
 end
