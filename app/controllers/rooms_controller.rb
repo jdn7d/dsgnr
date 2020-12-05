6c1bcdc91f10d9c8bb51ad97bc57a26fac3862ae
Jessie <jssdnvn.com>
2020-12-05 09:50:38 -0600

    deleted dupicate code in rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -23,21 +23,18 @@ class RoomsController < ApplicationController
    end
 
    def show 
-      @room = Room.find(params[:id])
+   
    end
 
    def edit
-      @room = Room.find(params[:id])
    end
 
    def update 
-      @room = Room.find(params[:id])
       @room.update(room_params) 
       redirect_to '/rooms'
    end
 
    def destroy 
-      @room = Room.find(params[:id])
       @room.destroy
       redirect_to rooms_path
    end
