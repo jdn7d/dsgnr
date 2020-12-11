d9cdc50fe5acb55829e3de9b7909dc3a6c17acb9
Jessie <jssdnvn.com>
2020-12-11 14:24:57 -0600

    added uniqueness and precense validation to room model and link to partial in room/form

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,8 @@
 class Room < ApplicationRecord
-  belongs_to :designer
-  has_many :reference_photos
-  has_many :pieces
-  has_many :stores, through: :pieces
+    validates :name, uniqueness: true, presence: true
+
+    belongs_to :designer
+    has_many :reference_photos
+    has_many :pieces
+    has_many :stores, through: :pieces
 end
