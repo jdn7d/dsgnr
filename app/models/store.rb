1c9d2428e6e483aad6793f415ddc18d9c7fe4fca
Jessie <jssdnvn.com>
2020-12-11 15:26:12 -0600

    corrected typo in store model

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ApplicationRecord
    validates :name, uniqueness: true 
-   scope :current_designers_rooms, -> { where(designer_id: current_deisgner) }
+   scope :current_designers_rooms, -> { where(designer_id: current_designer) }
    
    has_many :pieces
    has_many :rooms, through: :pieces 
